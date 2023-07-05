5f9a2b44aac01bf7816d388bad430f66d0637e70
daniel-macias <josedanielmr@gmail.com>
2023-07-05 11:04:11 -0600

    added seedrandom ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Map from './components/Map';
+import seedrandom from 'seedrandom';
 
 function App() {
+
+  const [randomNumber, setRandomNumber] = useState('');
+
+  useEffect(() => {
+    const now = new Date();
+    const rng = seedrandom(
+      (now.getDate() + now.getMonth() * 32 + now.getFullYear() * 400).toString()
+    );
+
+    setRandomNumber(rng().toString());
+
+
+  }, []); 
+
   return (
     <div className="App">
       <p>Landmass</p>
       <Map squareSize={1} boardSize={500} />
+      <p>{randomNumber}</p>
     </div>
   );
 }
