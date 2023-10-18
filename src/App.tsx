3534f81c68d83f59e45713962ced4835db015e04
daniel-macias <josedanielmr@gmail.com>
2023-10-18 10:22:52 -0600

    added new structure to the visuals and 2 new components

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import React, { useEffect, useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Map from './components/Map';
+import Settings from './components/Settings';
+import Explanation from './components/Explanation';
 import seedrandom from 'seedrandom';
+import { Grid, Paper, Container } from '@mui/material';
 import { createNoise2D } from 'simplex-noise';
 
 function App() {
@@ -22,9 +25,45 @@ function App() {
 
   return (
     <div className="App">
+      <Container maxWidth="lg">
+      <div
+        style={{
+          minHeight: '100vh', // Set a minimum height to fill the entire viewport
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center', // Center the content horizontally
+          overflowY: 'auto', // Enable vertical scrolling when needed
+        }}
+      >
+        
       <p>Landmass</p>
-      <Map squareSize={1} boardSize={500} seed={randomNumber} edgeCompressionAmount={0.1} decreasingMultiplier={0.4} increasingMultiplier={0.07} decreasingOffset={0.4} increasingOffset={0.7} scale={200} />
+      
       <p>{randomNumber}</p>
+      {/* 'lg' here sets the maximum width to large screens, but you can adjust this based on your preference */}
+      <Grid container spacing={2}>
+        {/* For larger screens (1/3 and 2/3) */}
+        
+        <Grid item xs={12} sm={8}>
+          {/* Content for the 2/3 column */}
+          <Paper elevation={3} style={{ minHeight: '200px', display: 'flex', flexDirection: 'column' }}>
+            <div style={{ flex: 1, overflow: 'auto' }}>
+            <Map squareSize={1} boardSize={500} seed={randomNumber} edgeCompressionAmount={0.1} decreasingMultiplier={0.4} increasingMultiplier={0.07} decreasingOffset={0.4} increasingOffset={0.7} scale={200} />
+            </div>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          {/* Content for the 1/3 column */}
+          <Paper elevation={3} style={{ minHeight: '200px', display: 'flex', flexDirection: 'column' }}>
+            1/3 Column Content
+            <Settings />
+            <Explanation />
+          </Paper>
+        </Grid>
+      </Grid>
+      </div>
+    </Container>
+      
+      
     </div>
   );
 }
